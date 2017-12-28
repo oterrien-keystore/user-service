a0a7e4f9feb70e4c6f266b86bcedb0a1538b76be
OTerrien <oterrien@neuf.fr>
2017-12-28 11:04:52 +0100

    remove unused imports

--- src/main/java/com/ote/user/security/CustomAuthenticationProvider.java
+++ src/main/java/com/ote/user/security/CustomAuthenticationProvider.java
@@ -9,8 +9,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.util.Collections;
 
 @Service
